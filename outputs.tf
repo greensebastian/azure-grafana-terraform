ccf6f477e17fc1015e8d6d7e859614fb3fa7089f
Sebastian Green <sebastianpetergreen@gmail.com>
2023-11-09 20:12:12 +0100

    VM data in grafana, user with access

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,19 @@ output "resource_group_name" {
 output "public_ip_address" {
   value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
 }
+
+output "grafana_user_principal_name" {
+  value = azuread_user.grafana_admin.user_principal_name
+}
+
+output "grafana_user_password" {
+  value = nonsensitive(azuread_user.grafana_admin.password)
+}
+
+output "vm_admin_username" {
+  value = var.username
+}
+
+output "vm_admin_ssh_key" {
+  value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).privateKey
+}
--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">=0.12"
+  required_version = "~>1.0"
 
   required_providers {
     azapi = {
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~>3.0"
     }
     random = {
       source  = "hashicorp/random"
